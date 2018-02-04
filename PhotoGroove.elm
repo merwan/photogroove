57c5c84e343f680f68c359074d404da8350f5a83
Merouane Atig <merwan@patem.net>
2018-02-05 00:15:40 +0100

    Use a model

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -5,15 +5,26 @@ import Html exposing (..)
 import Html.Attributes exposing(..)
 
 
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+        , div [ id "thumbnails" ] (List.map viewThumbnail initialModel)
         ]
 
+viewThumbnail thumbnail =
+    img [ src (urlPrefix ++ thumbnail.url) ] []
+
+
 main =
-    view "no model yet"
+    view initialModel
