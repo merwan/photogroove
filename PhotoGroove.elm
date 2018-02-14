2e7a52b56fb338d11155451e2c5b6a4aa91f2133
Merouane Atig <merwan@patem.net>
2018-02-14 22:42:07 +0100

    Add surprise me button

--- PhotoGroove.elm
+++ PhotoGroove.elm
@@ -1,12 +1,23 @@
 module PhotoGroove exposing (..)
 
 
+import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing(..)
 import Html.Events exposing(onClick)
 
 
-initialModel : { photos : List { url : String }, selectedUrl : String }
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+
+
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -16,15 +27,27 @@ initialModel =
     , selectedUrl = "1.jpeg"
     }
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
 
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+type alias Msg =
+    { operation : String, data : String }
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
+        , button
+            [ onClick { operation = "SURPRISE_ME", data = "" } ]
+            [ text "Surprise Me!" ]
         , div [ id "thumbnails" ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img
@@ -34,6 +57,7 @@ view model =
             []
         ]
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
@@ -43,11 +67,17 @@ viewThumbnail selectedUrl thumbnail =
         []
 
 
+update : Msg -> Model -> Model
 update msg model =
-    if msg.operation == "SELECT_PHOTO" then
-        { model | selectedUrl = msg.data }
-    else
-        model
+    case msg.operation of
+        "SELECT_PHOTO" ->
+            { model | selectedUrl = msg.data }
+
+        "SURPRISE_ME" ->
+            { model | selectedUrl = "2.jpeg" }
+
+        _ ->
+            model
 
 
 main =
